71daab568cc5cc63b5ebb57e3d27493da7a9eaa5
Kotaro Isobe <kisobe0609@gmail.com>
2021-03-23 22:49:01 +0900

    edit App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import QRCode from "qrcode.react";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello React Native!!　りあくとねいてぃぶ
       </Text>
+      <QRCode value="http://facebook.com/" />
       <StatusBar style="auto" />
     </View>
   );
